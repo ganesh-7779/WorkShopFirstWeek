e24cacaf3926151f6e4663e8b395d4351f72005a
Ganesh <ganeshgavhad9@gmail.com>
2021-07-08 10:13:17 +0530

    initial commit

---
+++ TicTacToeGame.java
@@ -0,0 +1,15 @@
+
+/*******************************************************
+ * Purpose : Creating Board for Tic Tac Toe Game Program
+ * @author Ganesh Gavhad
+ *******************************************************/
+package com.bridgelabz;
+
+public class TicTacToeGame {
+	public static char[] board = new char[10];
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Tic Tac Toe Game");
+	
+}
+}
